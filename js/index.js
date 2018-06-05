8e72cbaef3e953dc0d69e5bfd1aaa7949be984ae
frank402 <frank40202@gmail.com>
2018-06-05 15:15:17 +0800

    L3/L5 complete

--- js/index.js
+++ js/index.js
@@ -1,89 +1,125 @@
 class BaseCharacter {
     constructor(name, hp, ap) {
-        this.name = name;
-        this.hp = hp;
-        this.maxHp = hp;
-        this.ap = ap;
-        this.alive = true;
+      this.name = name;
+      this.hp = hp;
+      this.maxHp = hp;
+      this.ap = ap;
+      this.alive = true;
     }
     attack(character, damage) {
-        if (this.alive == false) {
-            return;
-        }
-        character.getHurt(damage);
+      if (this.alive == false) {
+        return;
+      }
+      character.getHurt(damage);
     }
     getHurt(damage) {
-        this.hp -= damage;
-        if (this.hp <= 0) {
-            this.die();
-        }
+      this.hp -= damage;
+      if (this.hp <= 0) {
+        this.die();
+      }
     }
     die() {
-        this.alive = false;
+      this.alive = false;
     }
+    //血條更新
     updateHtml(hpElement, hurtElement) {
-        hpElement.textContent = this.hp;
-        hurtElement.style.width = (100 - this.hp / this.maxHp * 100) + "%";
+      hpElement.textContent = this.hp;
+      hurtElement.style.width = (100 - this.hp / this.maxHp * 100) + "%";
     }
-}
-
-class Hero extends BaseCharacter {
+  }
+  
+  class Hero extends BaseCharacter {
     constructor(name, hp, ap) {
-        super(name, hp, ap);
-
-        this.element = document.getElementById("hero-image-block");
-        this.hpElement = document.getElementById("hero-hp");
-        this.maxHpElement = document.getElementById("hero-max-hp");
-        this.hurtElement = document.getElementById("hero-hp-hurt");
-
-        this.hpElement.textContent = this.hp;
-        this.maxHpElement.textContent = this.maxHp;
-
-        console.log("召喚英雄 " + this.name + "！");
+      super(name, hp, ap);
+  
+      this.element = document.getElementById("hero-image-block");
+      this.hpElement = document.getElementById("hero-hp");
+      this.maxHpElement = document.getElementById("hero-max-hp");
+      this.hurtElement = document.getElementById("hero-hp-hurt");
+  
+      this.hpElement.textContent = this.hp;
+      this.maxHpElement.textContent = this.maxHp;
+  
+      console.log("召喚英雄 " + this.name + "！");
     }
     attack(character) {
-        var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
-        super.attack(character, Math.floor(damage));
+      var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
+      super.attack(character, Math.floor(damage));
     }
     getHurt(damage) {
-        super.getHurt(damage);
-        this.updateHtml(this.hpElement, this.hurtElement);
-      }
-}
-
-class Monster extends BaseCharacter {
+      super.getHurt(damage);
+      this.updateHtml(this.hpElement, this.hurtElement);
+    }
+  }
+  
+  class Monster extends BaseCharacter {
     constructor(name, hp, ap) {
-        super(name, hp, ap);
-        console.log("遇到怪獸 " + this.name + "了！");
+      super(name, hp, ap);
+  
+      this.element = document.getElementById("monster-image-block");
+      this.hpElement = document.getElementById("monster-hp");
+      this.maxHpElement = document.getElementById("monster-max-hp");
+      this.hurtElement = document.getElementById("monster-hp-hurt");
+  
+      this.hpElement.textContent = this.hp;
+      this.maxHpElement.textContent = this.maxHp;
+  
+      console.log("遇到怪獸 " + this.name + "了！");
     }
     attack(character) {
-        var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
-        super.attack(character, Math.floor(damage));
+      var damage = Math.random() * (this.ap / 2) + (this.ap / 2);
+      super.attack(character, Math.floor(damage));
     }
-}
-
-
-
-var hero = new Hero("Bernard", 130, 30);
-var monster = new Monster("Skeleton", 130, 10);
-
-function addSkillEvent() {
+    getHurt(damage) {
+      super.getHurt(damage);
+      this.updateHtml(this.hpElement, this.hurtElement);
+    }
+  }
+  
+  var rounds = 10;
+  var hero = new Hero("Bernard", 130, 30);
+  var monster = new Monster("Skeleton", 130, 10);
+  
+  function endTurn() {
+    rounds--;
+    document.getElementById("round-num").textContent = rounds;
+    if (rounds < 1) {
+  
+    }
+  }
+  
+  function heroAttack() {
+    // Hero 選技能時觸發回合開始
+    document.getElementsByClassName("skill-block")[0].style.display = "none";
+  
+    setTimeout(function() {
+      hero.element.classList.add("attacking");
+      setTimeout(function() {
+        hero.attack(monster);
+        hero.element.classList.remove("attacking");
+      }, 500);
+    }, 100);
+  
+    setTimeout(function() {
+      if (monster.alive) {
+        monster.element.classList.add("attacking")
+        setTimeout(function() {
+          monster.attack(hero);
+          monster.element.classList.remove("attacking");
+          endTurn();
+          if (hero.alive == false) {} else {
+            document.getElementsByClassName("skill-block")[0].style.display = "block";
+          }
+        }, 500);
+      } else {}
+    }, 1100);
+  
+  }
+  
+  function addSkillEvent() {
     var skill = document.getElementById("skill");
-    skill.onclick = function() { 
-      heroAttack(); 
+    skill.onclick = function() {
+      heroAttack();
     }
   }
   addSkillEvent();
-
-  var rounds = 10;
-function endTurn() {
-  rounds--;
-  document.getElementById("round-num").textContent = rounds;
-  if (rounds < 1) {
-    // 「遊戲結束」空白區
-  }
-}
-
-function heroAttack() {
-    document.getElementsByClassName("skill-block")[0].style.display = "none";
-  }
